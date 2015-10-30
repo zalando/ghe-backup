c97fb2b94ee8da21aa06c5c5c9d665293fabdfc2
lotharschulz <lothar.sch@gmx.de>
2015-10-30 23:15:52 +0100

    if changed

--- travis.sh
+++ travis.sh
@@ -6,7 +6,7 @@ cd bashtest
 sudo ./prepare-tests.sh
 SSHKEY="aws:kms:myAWSregion:123456789:key/myrandomstringwithnumbers123456567890"
 
-if [ $SSHKEY == "aws:kms:"* ]; then
+if [ $SSHKEY = "aws:kms:"* ]; then
   SSHKEY=${SSHKEY##aws:kms:}
   SSHKEY=`python3 /kms/decryptkms.py $SSHKEY`
   if [[ $SSHKEY == "Invalid KMS key." ]]
