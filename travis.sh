1f4901db96f814be837e5bca3ceaf4700b4e3a0e
lotharschulz <lothar.sch@gmx.de>
2015-10-30 23:07:59 +0100

    another apt-get attempt

--- travis.sh
+++ travis.sh
@@ -2,16 +2,37 @@ set +e
 # http://stackoverflow.com/questions/13400445/jenkins-build-script-exits-after-google-test-execution/13404608#13404608
 # http://stackoverflow.com/questions/22814559/how-when-does-execute-shell-mark-a-build-as-failure-in-jenkins
 
-#virtualenv --no-site-packages --distribute -p /usr/bin/python3 ~/.ghebackup_venv
-#. ~/.ghebackup_venv/bin/activate
-#pip install boto3 --upgrade
-#pip install pyyaml --upgrade
-#pip install requests --upgrade
-
 cd bashtest
 sudo ./prepare-tests.sh
-sudo chmod 0755 /kms/parseyaml.py
-../convert-kms-private-ssh-key.sh
+SSHKEY="aws:kms:myAWSregion:123456789:key/myrandomstringwithnumbers123456567890"
+
+if [[ $SSHKEY == "aws:kms:"* ]]; then
+  SSHKEY=${SSHKEY##aws:kms:}
+  SSHKEY=`python3 /kms/decryptkms.py $SSHKEY`
+  if [[ $SSHKEY == "Invalid KMS key." ]]
+  then
+    echo "KMS key or KMS string is invalid."
+    echo "KMS string must be formate: aws:kms:<BASE64STRING>"
+    echo "KMS key must be usable via Host-IAM-Profile"
+    exit 1
+  fi
+
+  if [ -f ~/.ssh/id_rsa ]
+  then
+    echo "The file ~/.ssh/id_rsa exists already. Won't be overridden." >&2
+    exit 0
+  else
+    # assumption: file does not exists on new created docker container
+    echo "The file ~/.ssh/id_rsa does not exists. Start writing private ssh key.".
+    mkdir -p ~/.ssh
+    printf "%s" "$SSHKEY" >> ~/.ssh/id_rsa
+    chmod 0600 ~/.ssh/id_rsa
+    echo "Private ssh key file written.".
+    exit 0
+  fi
+fi
+exit 1
+
 
 if [ $? -eq 1 ]
 then
