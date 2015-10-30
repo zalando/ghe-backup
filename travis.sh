33826fe82accb001542e4c3e17e8e6fd186ace21
lotharschulz <lothar.sch@gmx.de>
2015-10-30 19:34:26 +0100

    zalando/ghe-backup#1 try w/o virtualenv

--- travis.sh
+++ travis.sh
@@ -2,11 +2,11 @@ set +e
 # http://stackoverflow.com/questions/13400445/jenkins-build-script-exits-after-google-test-execution/13404608#13404608
 # http://stackoverflow.com/questions/22814559/how-when-does-execute-shell-mark-a-build-as-failure-in-jenkins
 
-virtualenv --no-site-packages --distribute -p /usr/bin/python3 ~/.ghebackup_venv
-. ~/.ghebackup_venv/bin/activate
-pip install boto3 --upgrade
-pip install pyyaml --upgrade
-pip install requests --upgrade
+#virtualenv --no-site-packages --distribute -p /usr/bin/python3 ~/.ghebackup_venv
+#. ~/.ghebackup_venv/bin/activate
+#pip install boto3 --upgrade
+#pip install pyyaml --upgrade
+#pip install requests --upgrade
 
 cd bashtest
 ./prepare-tests.sh
