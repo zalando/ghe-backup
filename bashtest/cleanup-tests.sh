0fcd671868d2c18e5af305b8ea0f760afac1162c
lotharschulz <lothar.sch@gmx.de>
2015-11-03 09:47:05 +0100

    test files gets emptied

--- bashtest/cleanup-tests.sh
+++ bashtest/cleanup-tests.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 # clean up test files (before-convert-kms-private-ssh-key.sh)
-
-rm -rf /kms/
-rm -rf /meta/
+files=("/kms/parseyaml.py" "/kms/decryptkms.py" "/meta/taupage.yaml" )
+for file in "${files[@]}"
+do
+   :
+   #echo $file
+   if [ -f "$file" ]; then
+     #echo "$file does exists"
+     echo -n "" > $file
+   else
+      echo "$file does not exists"
+   fi
+done
