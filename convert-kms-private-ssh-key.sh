d203ec2b8877329837df3c223d7211e32cebb2a1
lotharschulz <lothar.sch@gmx.de>
2015-11-16 13:12:40 +0100

    saved some lines calling extract-kms-str.py

--- convert-kms-private-ssh-key.sh
+++ convert-kms-private-ssh-key.sh
@@ -4,32 +4,28 @@ set -euo pipefail
 IFS=$'\n\t'
 
 #read senza yaml file (taupage.yaml)
-eval $(/kms/parseyaml.py --export /meta/taupage.yaml "configuration")
-SSHKEY=$(echo "$configuration_SenzaComponents" | python3 -c 'import sys;stuff=sys.stdin.read(1000);kmsstuff=stuff[stuff.find("aws:kms:"):len(stuff)];kmsstr=kmsstuff[0:kmsstuff.find(",")-1];print(kmsstr + "\n")')
-
-if [[ $SSHKEY == "aws:kms:"* ]]; then
-  SSHKEY=${SSHKEY##aws:kms:}
-  SSHKEY=`python3 /kms/decryptkms.py $SSHKEY`
-  if [[ $SSHKEY == "Invalid KMS key." ]]
-  then
-    echo "KMS key or KMS string is invalid."
-    echo "KMS string must be formate: aws:kms:<BASE64STRING>"
-    echo "KMS key must be usable via Host-IAM-Profile"
-    exit 1
-  fi
-
-  if [ -f ~/.ssh/id_rsa ]
-  then
-    echo "The file ~/.ssh/id_rsa exists already. Won't be overridden." >&2
-    exit 0
-  else
-    # assumption: file does not exists on new created docker container
-    echo "The file ~/.ssh/id_rsa does not exists. Start writing private ssh key.".
-    mkdir -p ~/.ssh
-    printf "%s" "$SSHKEY" >> ~/.ssh/id_rsa
-    chmod 0600 ~/.ssh/id_rsa
-    echo "Private ssh key file written.".
-    exit 0
-  fi
+SSHKEY=$(python3 /kms/extract-kms-str.py /meta/taupage.yaml -r "aws:kms:.*?," -p "" -s "',")
+SSHKEY=`python3 /kms/decryptkms.py $SSHKEY`
+if [[ $SSHKEY == "Invalid KMS key." ]]
+then
+  echo "KMS key or KMS string is invalid."
+  echo "KMS string must be formate: aws:kms:<BASE64STRING>"
+  echo "KMS key must be usable via Host-IAM-Profile"
+  exit 1
 fi
+
+if [ -f ~/.ssh/id_rsa ]
+then
+  echo "The file ~/.ssh/id_rsa exists already. Won't be overridden." >&2
+  exit 0
+else
+  # assumption: file does not exists on new created docker container
+  echo "The file ~/.ssh/id_rsa does not exists. Start writing private ssh key.".
+  mkdir -p ~/.ssh
+  printf "%s" "$SSHKEY" >> ~/.ssh/id_rsa
+  chmod 0600 ~/.ssh/id_rsa
+  echo "Private ssh key file written.".
+  exit 0
+fi
+
 exit 1
