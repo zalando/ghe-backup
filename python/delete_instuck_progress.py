a9bbc3bcec9f7c6dfd2493920e520ef3ffbd7e42
lotharschulz <lothar.schulz@zalando.de>
2016-07-21 18:16:57 +0200

    https://github.com/zalando/ghe-backup/issues/20 drop_outdated_inprogress_file returns True if file was deleted

--- python/delete_instuck_progress.py
+++ python/delete_instuck_progress.py
@@ -7,8 +7,6 @@ import os
 import os.path
 from datetime import datetime
 from datetime import date
-# import time
-# from datetime import timedelta
 
 
 def drop_file(abs_folder, filename):
@@ -20,59 +18,17 @@ def drop_file(abs_folder, filename):
 
 def drop_outdated_inprogress_file(abs_folder, filename):
     if os.path.isfile(os.path.join(abs_folder, filename)):
-        # modification_time = os.stat(os.path.join(abs_folder, filename)).st_mtime
-        # now = time.gmtime()
-        # print("\nmtime: " + str(modification_time))
-        # print("now: " + str(now))
-        # dtnow = datetime.fromtimestamp(time.mktime(now))
-        # print("dtnow: " + str(dtnow))
-        # dtnow2 = datetime.now()
-        # print("dtnow2: " + str(dtnow2))
-        # dtnow2_days = datetime.strptime(str(dtnow2), '%Y-%m-%d')
-        # print("dtnow2_days: " + str(dtnow2_days))
-        #
-        # modification_datetime=datetime.fromtimestamp(modification_time).strftime('%Y-%m-%d %H-%M-%S.%f')
-        # print("modification_datetime: " + modification_datetime)
-
         modification_time = os.stat(os.path.join(abs_folder, filename)).st_mtime
         modification_time_year = int(datetime.fromtimestamp(modification_time).strftime('%Y'))
         modification_time_month = int(datetime.fromtimestamp(modification_time).strftime('%m'))
         modification_time_day = int(datetime.fromtimestamp(modification_time).strftime('%d'))
-        print("\nmodification_time_year: " + str(modification_time_year))
-        print("modification_time_month: " + str(modification_time_month))
-        print("modification_time_day: " + str(modification_time_day))
 
         today = date.today()
-        # modification_day = date(2008, 12, 25)
         modification_day = date(modification_time_year, modification_time_month, modification_time_day)
 
-        print("\ndiff.days: " + str((today - modification_day).days))
         if (today - modification_day).days > 1:
-            print("\nabout to remove: " + os.path.join(abs_folder, filename))
             os.remove(os.path.join(abs_folder, filename))
-            print("\nos.path.isfile(os.path.join(abs_folder, filename)): " + str(os.path.isfile(os.path.join(abs_folder, filename))))
-
-        # delta = dtnow2 - modification_datetime
-        # print("delta" + str(delta))
-        # print("delta.days" + str(delta.days))
-
-        # http://stackoverflow.com/questions/8022161/python-converting-from-datetime-datetime-to-time-time
-        # http://stackoverflow.com/questions/12400256/python-converting-epoch-time-into-the-datetime
-
-        # TODO
-        # check last modification of os.path.join(abs_folder, filename)
-        #  smth like os.stat(os.path.join(abs_folder, filename)).st_mtime
-        # check current time
-        #  smth like now = time.gmtime()
-        # compare both timestamps
-        #  smth like
-        #   nowvalue = datetime.datetime.fromtimestamp(calendar.timegm(now))
-        #   mtimevalue = datetime.datetime.fromtimestamp(calendar.timegm(mtime))
-        #   if datetime.timedelta(seconds=t2-t1) > one day:
-        #     go delete
-
-        #return True
-        return False
+            return True
     return False
 
 if __name__ == "__main__":
--- python/test_delete_instuck_progress.py
+++ python/test_delete_instuck_progress.py
@@ -32,8 +32,7 @@ def test_drop_file(abs_folder='~/3456', filename='test.txt'):
 def test_drop_outdated_inprogress_file(abs_folder='~', filename='in-progress'):
     create_test_files('~', 'in-progress')
     res = delete_instuck_progress.drop_outdated_inprogress_file(abs_folder, filename)
-    # nt.assert_equal(True, res)
-    nt.assert_equal(False, res)
+    nt.assert_equal(True, res)
 
 
 def cleanup_test_files():
