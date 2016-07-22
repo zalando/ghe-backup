70599c4ed39fc65378da4655e8b0c1adeab9e9d7
lotharschulz <lothar.schulz@zalando.de>
2016-07-22 23:28:00 +0200

    https://github.com/zalando/ghe-backup/issues/20 import tempfile as tf

--- python/test_delete_instuck_progress.py
+++ python/test_delete_instuck_progress.py
@@ -6,7 +6,7 @@ import nose.tools as nt
 import delete_instuck_progress
 import datetime
 import time
-import tempfile
+import tempfile as tf
 
 
 # note: ~ folder is not your home folder, but a sub folder of the one this code runs into
@@ -25,7 +25,7 @@ def create_test_files(abs_folder='~/tmp', filename='in-progress'):
 
 
 def test_drop_outdated_inprogress_file(filename='in-progress'):
-    with tempfile.TemporaryDirectory() as tmp_dir:
+    with tf.TemporaryDirectory() as tmp_dir:
         create_test_files(tmp_dir, filename)
         res = delete_instuck_progress.drop_outdated_inprogress_file(tmp_dir, filename)
         nt.assert_equal(True, res and not os.path.exists(os.path.join(tmp_dir, filename)))
--- travis_deleteInstuckProgress.sh
+++ travis_deleteInstuckProgress.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nosetests -w python -v --nocapture test_delete_instuck_progress.py
+nosetests -w python3 -v --nocapture test_delete_instuck_progress.py
