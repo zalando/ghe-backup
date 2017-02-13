9925bb90c57c8ff2e292c52396ed489d947a594e
lotharschulz <lothar.schulz@zalando.de>
2017-02-13 17:12:17 +0100

    pep8 adaptions

--- python/test_trigger_ghebackup.py
+++ python/test_trigger_ghebackup.py
@@ -4,7 +4,7 @@ import trigger_ghebackup
 import nose.tools as nt
 
 
-def test_trigger_backupt(pname="bash /backup/backup-utils/bin/ghe-backup"):
+def test_trigger_backup(pname="bash /backup/backup-utils/bin/ghe-backup"):
     nt.assert_true("/backup/backup-utils/bin/ghe-backup" == trigger_ghebackup.trigger_backup(pname, True),
                    "'" + pname + "' should have been started.")
 
@@ -14,6 +14,6 @@ def test_trigger_nano(pname="nano"):
                    "'" + pname + "' should have been started.")
 
 
-def test_trigger_bash(pname="bash"): # assuming tests run on *nix
+def test_trigger_bash(pname="bash"):  # assuming tests run on *nix
     nt.assert_false(pname == trigger_ghebackup.trigger_backup(pname, True),
                     "'" + pname + "' should have been _not_ started.")
