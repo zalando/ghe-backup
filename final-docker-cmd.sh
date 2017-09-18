c3b755c8f7d654c75cf03971bc0929b8520f98cb
lotharschulz <lothar.schulz@zalando.de>
2017-09-18 16:24:01 +0200

    more debugs

--- final-docker-cmd.sh
+++ final-docker-cmd.sh
@@ -5,8 +5,13 @@ IFS=$'\n\t'
 
 echo $PATH
 env
-/usr/bin/python3 /delete-instuck-backups/delete_instuck_progress.py
+echo "========================="
+which python3
+echo "run /delete-instuck-backups/delete_instuck_progress.py"
+python3 /delete-instuck-backups/delete_instuck_progress.py
+echo "run /kms/convert-kms-private-ssh-key.sh"
 /kms/convert-kms-private-ssh-key.sh
+echo "run cron"
 # do the actual backups via cron
 cron
 tail -F /var/log/ghe-prod-backup.log
