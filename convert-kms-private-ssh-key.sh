928166b23cfbe6ad9a4e0cf335ae236ecbf39ffe
lotharschulz <lothar.schulz@zalando.de>
2017-09-26 13:55:02 +0200

    readlink and ls for all sym links

--- convert-kms-private-ssh-key.sh
+++ convert-kms-private-ssh-key.sh
@@ -33,7 +33,8 @@ then
   echo "check for /meta/ghe-backup-secret directory. ls /meta/ghe-backup-secret: "
   ls -lisa /meta/ghe-backup-secret
   echo "cat kms_private_ssh_key: "
-  cat ../data/kms_private_ssh_key
+  readlink kms_private_ssh_key
+  find . -type l -exec ls -la {} \;
   cat ..data/kms_private_ssh_key
   exit 1
 else
