120dc827e4c27de8f67d36b93935a2cd1c7f01d7
lotharschulz <lothar.schulz@zalando.de>
2017-09-26 13:41:41 +0200

    cat kms key file

--- convert-kms-private-ssh-key.sh
+++ convert-kms-private-ssh-key.sh
@@ -30,8 +30,10 @@ if [ -f /details/labels ]
 then
   echo "File /details/labels exists. cat /details/labels: "
   cat /details/labels
-  echo "check for /meta/ghe-backup-secret file. cat /meta/ghe-backup-secret: "
+  echo "check for /meta/ghe-backup-secret directory. ls /meta/ghe-backup-secret: "
   ls -lisa /meta/ghe-backup-secret
+  echo "cat kms_private_ssh_key: "
+  cat kms_private_ssh_key
   exit 1
 else
   echo "File /details/labels does not exist."
