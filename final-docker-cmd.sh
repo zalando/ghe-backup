b5bcccd7d4ea97741861d5e064a43cbe619d1f82
lotharschulz <lothar.schulz@zalando.de>
2017-09-29 19:06:48 +0200

    adding debug lines

--- final-docker-cmd.sh
+++ final-docker-cmd.sh
@@ -3,15 +3,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-#echo $PATH
-#env
-#echo "======================================================"
-#which python3
-#echo "run /delete-instuck-backups/delete_instuck_progress.py"
 python3 /delete-instuck-backups/delete_instuck_progress.py
-#echo "run /kms/convert-kms-private-ssh-key.sh"
+echo "run /kms/convert-kms-private-ssh-key.sh"
 /kms/convert-kms-private-ssh-key.sh
-#echo "run cron"
+echo "run cron"
 # do the actual backups via cron
 cron
 tail -F /var/log/ghe-prod-backup.log
