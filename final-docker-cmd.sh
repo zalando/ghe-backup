e165d9304040df31930e8f398a1c743c5e342557
lotharschulz <lothar.schulz@zalando.de>
2017-10-29 23:32:56 +0100

    change echos

--- final-docker-cmd.sh
+++ final-docker-cmd.sh
@@ -5,8 +5,7 @@ IFS=$'\n\t'
 
 python3 /delete-instuck-backups/delete_instuck_progress.py
 /kms/convert-kms-private-ssh-key.sh
-echo "run cron"
 # do the actual backups via cron
 cron
-echo "after cron"
+echo "start tailing logs"
 tail -F /var/log/ghe-prod-backup.log
