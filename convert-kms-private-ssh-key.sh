1f4f5809a338f36fa12f61db624405ed6e1607d6
lotharschulz <lothar.schulz@zalando.de>
2017-10-30 12:36:02 +0100

    drop comments

--- convert-kms-private-ssh-key.sh
+++ convert-kms-private-ssh-key.sh
@@ -13,27 +13,9 @@ else
 fi
 set -u
 
-# https://stackoverflow.com/questions/36639062/how-do-i-tell-if-my-container-is-running-inside-a-kubernetes-cluster
-# - use https://kubernetes.io/docs/tasks/inject-data-application/downward-api-volume-expose-pod-information/ to check if container runs on K8s
-#  - Motivation for the Downward API
-#  - "you can see that the labels and annotations files are in a temporary subdirectory"
-#  - pod fields and container fields (symbolic links"
-#  - "Using symbolic links enables dynamic atomic refresh of the metadata; updates are written to a new temporary directory"
-# - https://lukemarsden.github.io/docs/user-guide/downward-api/
-#  - "Downward API volume permits to store more complex data like metadata.labels and metadata.annotations"
-# - if K8s
-# - mount secrets
-# - read secret file and write the public key
-# - else
-
-# running as pod that gets labels file via downward-api-volume
-# can be done with ```if [ -d /meta/ghe-backup-secret ];``` .... as well
-
 if [ -f /details/labels ]
 then
-  #echo "head -10 /meta/ghe-backup-secret/kms_private_ssh_key: "
-  #head -10 /meta/ghe-backup-secret/kms_private_ssh_key
-  ### @TODO: separate function parameter would be private key content ($SSHKEY /meta/ghe-backup-secret/kms_private_ssh_key)
+  # @TODO: separate function parameter would be private key content ($SSHKEY /meta/ghe-backup-secret/kms_private_ssh_key)
   if [ -f ~/.ssh/id_rsa ]
   then
     echo "The file ~/.ssh/id_rsa exists already. Won't be overridden." >&2
