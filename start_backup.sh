f196a46e3e666c829c30da69b057e65f4d710cc5
lotharschulz <lothar.schulz@zalando.de>
2017-11-09 15:28:04 +0100

    change ownership of /data at runtime of not owned by application user

--- start_backup.sh
+++ start_backup.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 pidof -o $$ -x "$0" >/dev/null 2>&1 && exit 1
+if [ "root"  ==  "`stat -c '%U' /data`" ]
+then
+    sudo chown -R application: /data
+fi
 /backup/backup-utils/bin/ghe-backup 1>> /var/log/ghe-prod-backup.log 2>&1
