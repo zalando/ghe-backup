8e307797b3794e2ccc06ca513f87edc73d4ae598
lotharschulz <lothar.schulz@zalando.de>
2017-11-15 13:20:09 +0100

    /data folder handling moved to start_backup shell script because on runtim of the shell script the volumes are mounted

--- start_backup.sh
+++ start_backup.sh
@@ -4,7 +4,11 @@ set -euo pipefail
 IFS=$'\n\t'
 
 pidof -o $$ -x "$0" >/dev/null 2>&1 && exit 1
-if [ "root"  ==  $(stat -c '%U' /data) ]
+if [ ! -d "/data/ghe-production-data/" ]
+then
+    mkdir -p /data/ghe-production-data/
+fi
+if [ 0  ==  $(stat -c '%u' /data) ]
 then
     sudo chown -R application: /data
 fi
