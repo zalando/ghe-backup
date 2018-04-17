ae4d65a5e7a8210ef73dc737baf5f3dfa94d9e96
Rasha Malek <rasha.malek@zalando.de>
2018-04-17 11:28:57 +0200

    issue #83 kms decryption region independent

--- convert-kms-private-ssh-key.sh
+++ convert-kms-private-ssh-key.sh
@@ -33,7 +33,7 @@ then
 elif [ -f $folder/taupage.yaml ]
 then
   echo "File $folder/taupage.yaml exists."
-  SSHKEY=$(python3 /kms/extract_decrypt_kms.py -f "$folder/taupage.yaml" -k "kms_private_ssh_key" -r "eu-west-1")
+  SSHKEY=$(python3 /kms/extract_decrypt_kms.py -f "$folder/taupage.yaml" -k "kms_private_ssh_key" -r "###REGION###")
   if [[ $SSHKEY == "Invalid KMS key." ]]
   then
     echo "KMS key or KMS string is invalid."
--- final-docker-cmd.sh
+++ final-docker-cmd.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 python3 /delete-instuck-backups/delete_instuck_progress.py
+REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}'`
+sed  "s/###REGION###/$REGION/g" /kms/convert-kms-private-ssh-key_original.sh
 /kms/convert-kms-private-ssh-key.sh
 # do the actual backups via cron
 # everything in sbin directory needs to be executed as privileged user
