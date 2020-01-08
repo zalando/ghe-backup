0dcd60e33f08755450ee0be6cd5e5d6ea5f7dd2e
Serhii Cherniavskyi <scherniavsky@ZALANDO-31329.corp.ad.zalando.net>
2020-01-08 12:15:23 +0100

    log to stdout of final-docker-cmd.sh

--- start_backup.sh
+++ start_backup.sh
@@ -12,4 +12,4 @@ if [ ! -d "/data/ghe-production-data/" ]
 then
     mkdir -p /data/ghe-production-data/
 fi
-/backup/backup-utils/bin/ghe-backup 2>&1
+/backup/backup-utils/bin/ghe-backup 2>&1 > /var/log/application.log
